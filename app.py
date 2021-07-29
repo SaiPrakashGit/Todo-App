bedc209a9cbe1a598b6af1c0058ef0f83d5927c1
SaiPrakashGit <prakashrayapati146@gmail.com>
2021-07-29 15:39:36 +0530

    This vesion creates the sql database with the given data format in conjunction with the app when it runs

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Configuring the database for the app (Here, I am using SQLAlchemy as I found is easy to configure)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+# Creates database file in current folder(we gave the relative path) while running the app
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Now, we create a class for the todo list item which has properties like id, name and state(finished or not)
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(50))			# Maximum of 50 characters
+	state = db.Column(db.Boolean) 			# Only 2 states  ( finished/not finished )
+
 
 @app.route("/")
 def main():
@@ -8,4 +23,5 @@ def main():
 	
 	
 if __name__ == "__main__" :
-	app.run()
+	db.create_all()						# Creates the data structure of given class
+	app.run(debug=True)
